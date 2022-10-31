2712c07de36ef7fa7e1db4700e296b2a42b2f3fa
Louis GAMBART <louis.gambart77270@gmail.com>
2022-10-31 11:42:54 +0100

    Fix mistake in unit of threshold Add snapshot size in output

--- VSS_Threshold.ps1
+++ VSS_Threshold.ps1
@@ -9,7 +9,7 @@ param(
 #
 # AUTHOR             :     Louis GAMBART
 # CREATION DATE      :     2022.10.17
-# RELEASE            :     v2.0.0
+# RELEASE            :     v2.1.0
 # USAGE SYNTAX       :     .\VSS_Threshold
 #
 # SCRIPT DESCRIPTION :     This script check the usage of VSS and print warning if it exceed threshold
@@ -19,6 +19,7 @@ param(
 #                 - RELEASE NOTES -
 # 1.0.0  2022.10.17 - Louis GAMBART - Initial version
 # 2.0.0  2022.10.31 - Louis GAMBART - Rework to follow Enable_VSS script
+# 2.1.0  2022.10.31 - Louis GAMBART - Fix mistake in unit of threshold, add snapshot size in output
 #
 #==========================================================================================
 
@@ -63,7 +64,8 @@ function getVSSusage {
         $DiskSize = [math]::Round([double]$volume.Size / 1GB, 3)
         $diskremaining = [math]::Round([double]$volume.SizeRemaining / 1GB, 3)
         if ($Disks.'Used (GB)' -gt $Threshold) {
-            "Disk $($Volume.DriveLetter) snapshot size is higher than $Threshold. The disk size is $($diskSize) and it has $($diskremaining) remaining space. The max snapshot size is $($Disks.'Max (GB)')"
+            Write-Host "Disk $($Volume.DriveLetter) snapshot size is higher than $Threshold. The disk size is $($diskSize) and it has $($diskremaining) remaining space. The max snapshot size is $($Disks.'Max (GB)')"
+            Write-Host "The snapshot size is $($Disks.'Used (GB)')"
         }
     }
     if (!$HealthState) {
@@ -81,8 +83,8 @@ function getVSSusage {
 #                          #
 ############################
 
-if ($Threshold -notmatch "^[0-9]+$") { Write-Host "Please enter a valid threshold number, in MB" }
+if ($Threshold -notmatch "^[0-9]+$") { Write-Host "Please enter a valid threshold number, in GB" }
 else {
     Write-Host "Starting script for $hostname"
-    getVSSusage -Threshold $Threshold"MB"
+    getVSSusage -Threshold $Threshold
 }
