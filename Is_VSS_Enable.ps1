110614a72f66885061ddc30242c396c146495f0b
Louis GAMBART <louis.gambart77270@gmail.com>
2022-10-31 11:21:34 +0100

    Rework to follow Enable_VSS script

--- Is_VSS_Enable.ps1
+++ Is_VSS_Enable.ps1
@@ -1,29 +1,41 @@
-# =======================================================
+#Requires -RunAsAdministrator
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$diskName
+)
+#==========================================================================================
 #
-# NAME: is_VSS_Enable.ps1
-# AUTHOR: GAMBART Louis
-# DATE: 20/10/2022
-# VERSION 1.5
+# SCRIPT NAME        :     is_VSS_Enable.ps1
 #
-# =======================================================
+# AUTHOR             :     Louis GAMBART
+# CREATION DATE      :     2022.10.18
+# RELEASE            :     v2.0.0
+# USAGE SYNTAX       :     .\is_VSS_Enable.ps1 -diskName "C"
 #
-# CHANGELOG
+# SCRIPT DESCRIPTION :     This script check if VSS is enable and send mail if not
 #
-# 1.0: Initial version
-# 1.1: Add the mail sending function
-# 1.2: Add the language check function to check if it is in english or french
-# 1.3: Refactor code, add try/catch for the MailMessage sending
-# 1.3.1: Add exit code when error is catched during the mail sending
-# 1.3.2: Add print of the maximum space that can be used by VSS
-# 1.4: Change language check verification
-# 1.5: Remove exit code for Centreon (this script already send mail and don't will be use with Centreon)
+#==========================================================================================
 #
-# =======================================================
+#                 - RELEASE NOTES -
+# 1.0.0  2022.10.18 - Louis GAMBART - Initial version
+# 1.1.0  2022.10.18 - Louis GAMBART - Add the mail sending function
+# 1.2.0  2022.10.18 - Louis GAMBART - Add the language check function to check if it is in english or french
+# 1.3.0  2022.10.19 - Louis GAMBART - Refactor code, add try/catch for the MailMessage sending
+# 1.3.1  2022.10.19 - Louis GAMBART - Add exit code when error is catched during the mail sending
+# 1.3.2  2022.10.19 - Louis GAMBART - Add print of the maximum space that can be used by VSS
+# 1.4.0  2022.10.19 - Louis GAMBART - Change language check verification
+# 1.5.0  2022.10.20 - Louis GAMBART - Remove exit code for Centreon (this script already send mail)
+# 1.6.0  2022.10.24 - Louis GAMBART - Remove Get-System-Language function and replace it by $PSUICulture variable (function don't return the value wanted)
+# 2.0.0  2022.10.31 - Louis GAMBART - Rework to follow Enable_VSS script
+#
+#==========================================================================================
 
 
-
-# ====================== VARIABLES ======================
-
+###################
+#                 #
+#  I - VARIABLES  #
+#                 #
+###################
 
 # clear $error variable
 $error.clear()
@@ -31,8 +43,91 @@ $error.clear()
 # get the name of the host
 $hostname = $env:COMPUTERNAME
 
-# cmdlet to execute
-$vss = cmd.exe /c "vssadmin list ShadowStorage"
+
+####################
+#                  #
+#  II - FUNCTIONS  #
+#                  #
+####################
+
+function Test-VSS {
+    <#
+    .SYNOPSIS
+    Check if VSS is enabled on the host
+    .DESCRIPTION
+    Check if VSS is enabled on the host through WMI Shadow Copy Object
+    .INPUTS
+    System.String: DiskName
+    .OUTPUTS
+    System.Boolean: return true if VSS is enabled on the host
+    .EXAMPLE
+    Test-VSS -DiskName "C"
+    True
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [String]$DiskName
+    )
+    begin { $vss = cmd.exe /c 'vssadmin list ShadowStorage' }
+    process {
+        if ($PSUICulture -eq "fr-FR") {
+            if ($vss -match "^(Il n'existe aucun )") { return $false }
+            else {
+                foreach ($line in $vss) {
+                    if ($line -match "Pour le volume : \(?($DiskName):\)\\\\\?\\Volume{(?<volume>[a-z0-9-]+)}\\") {
+                    return $true
+                    break
+                    }
+                }
+                return $false
+            }
+        }
+        else {
+            if ($vss -match "No shadow copies are configured") { return $false }
+            else {
+                foreach ($line in $vss) {
+                    if ($line -match "For volume: \(?($DiskName):\)\\\\\?\\Volume{(?<volume>[a-z0-9-]+)}\\") {
+                        return $true
+                        break
+                        }
+                    }
+                return $false
+            }
+        }
+    }
+    end {}
+}
+
+
+############################
+#                          #
+#  III - SCRIPT EXECUTION  #
+#                          #
+############################
+
+if ($diskName -match "^([a-zA-Z])$") {
+    if (Test-Path -Path $diskName":\") {
+        Write-Host "Starting script for $hostname"
+        if (Test-VSS -DiskName $diskName) {
+            Write-Host "VSS is enable on this host"
+            $result = "0"
+        }
+        else {
+            Write-Host "VSS is not enable on this host"
+            $result = "1"
+        }
+    }
+    else { Write-Host "Disk $diskName does not exist on $hostname" }
+}
+else { Write-Host "Disk $diskName is not a valid disk name, enter it just as a letter, like C or E" }
+
+
+######################
+#                    #
+# IV - MAIL SENDING  #
+#                    #
+######################
 
 # mail attributes
 $mail = @{
@@ -41,83 +136,20 @@ $mail = @{
     Cc = 'copy test mail'
     # Prod mail
     # To = 'prod mail', 'prod mail 2'
-    # Cc = 'copy prod mail' 
+    # Cc = 'copy prod mail'
     From = 'sender mail'
     Subject = '[VSS Monitoring]'
     Body = "Hello,
-    The server $hostname don't have VSS enable.
-    Do not reply to this email, it is automatically generated.
-    Cordially,
-    The Windows monitoring team."
+        The server $hostname don't have VSS enable.
+        Do not reply to this email, it is automatically generated.
+        Cordially,
+        The Windows monitoring team."
     SmtpServer = 'smtp server'
     ErrorAction = 'Stop'
 }
 
 # mail encoding
 $emailingEncoding = [System.Text.Encoding]::UTF8
-
-# ====================== FUNCTIONS ======================
-
-
-function Get-VSS-Status {
-    <#
-    .SYNOPSIS
-    Check if VSS service is enable on a host
-    .DESCRIPTION
-    The Get-VSS-Status function enable to know if the VSS service is enable on a host.
-    .INPUTS
-    vssadmin cmdlet query reply
-    .OUTPUTS
-    System.Int32: return 0 or 1 according to the status of the service
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true)]
-        $vss
-    )
-    if ($PSUICulture -eq "fr-FR") {
-        $vssMatch = "^(Il n'existe aucun )"
-    }
-    else {
-        $vssMatch = "No shadow copies are configured"
-    }
-
-    if ($vss -match $vssMatch) {
-        Write-Host "VSS is not enable"
-        return 1
-    }
-    else {
-        foreach ($line in $vss) {
-            if ($line -match "For volume: \((?<letter>[A-Z]):\)\\\\\?\\Volume{(?<volume>[a-z0-9-]+)}\\") {
-                $drive = $matches.letter
-                $volume = $matches.volume
-            }
-            if ($line -match "Used Shadow Copy Storage space: (.*) GB") { $used = $matches[1] }
-            if ($line -match "Allocated Shadow Copy Storage space: (.*) GB") { $allocated = $matches[1] }
-            if ($line -match "Maximum Shadow Copy Storage space: (.*) GB") { $maximum = $matches[1]}
-        }
-        Write-Host "VSS is enable on $drive drive and volume $volume"
-        Write-Host "Used space: $used GB"
-        Write-Host "Allocated space: $allocated GB"
-        Write-Host "Maximum space usable by VSS: $maximum"
-        return 0
-    }
-}
-
-
-# ======================== SCRIPT =======================
-
-
-Write-Host "Starting script for $hostname"
-$result = Get-VSS-Status -vss $vss
-
-
-# ====================== END SCRIPT =====================
-
-
-
-# ========================= MAIL ========================
-
 
 # Send mail if VSS is not enable
 if ($result -eq 1)
@@ -132,5 +164,3 @@ if ($result -eq 1)
         Write-Host "Mail sent"
     }
 }
-
-# ======================= END MAIL ======================
